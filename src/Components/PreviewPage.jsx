ebc81b753ccfa58deef7a2a1e821ee4cde9bf61b
Gucci Gangnam <diviner02hive@icloud.com>
2023-08-27 14:39:49 +0700

    Placing project ON HOLD

--- src/Components/PreviewPage.jsx
+++ src/Components/PreviewPage.jsx
@@ -4,16 +4,30 @@ import "./PreviewPage.css"
 export function PreviewBasicInfo({ fullName, phoneNumber, email, location }) {
     return <div className="previewBasicInfo">
         <h1>{fullName}</h1>
-        {phoneNumber}
-        {email}
-        {location}
-        </div>;
+        <div className="Basic-info">
+            ✉ {email}  |
+            ☎ {phoneNumber}  |
+            ⌂ {location}
+        </div>
+    </div>;
 }
 
 export function PreviewEducation({ schoolName, schoolYear, degree, score }) {
     return <div className="previewEducation">
         <h3>Education</h3>
-        
+        <div className="education-info">
+            <div className="school">
+                <b>{schoolName}</b>
+                <p></p>
+                {schoolYear}
+            </div>
+            <div className="degree">
+                <b>{degree}</b>
+                <p></p>
+                {score}
+            </div>
+        </div>
+
 
 
 
